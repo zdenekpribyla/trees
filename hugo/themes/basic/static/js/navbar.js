408e83966a14a1920778cae4c0c748767c33639b
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2019-05-08 07:15:44 +0200

    ad .js and index page

---
+++ hugo/themes/basic/static/js/navbar.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function () {
+
+	// ...
+
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function () {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+
+	// ...
+});
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if (element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if ($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({ scrollTop: scroll_to }, 1000);
+	}
+}
+
+jQuery(document).ready(function () {
+
+    /*
+        Navigation
+    */
+	$('a.scroll-link').on('click', function (e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+
+	// ...
+
+});
